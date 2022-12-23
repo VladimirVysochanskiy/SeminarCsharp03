c43833ab8224d0376b6b34cfccb1b917b2627938
VladimirVysochanskiy <shunkar78@mail.ru>
2022-12-23 18:53:31 +0300

    Add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+//возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+System.Console.Write("Введите номер четверти: ");
+int flat = Convert.ToInt32(Console.ReadLine());
+string[] part = {"x > 0; y > 0", "x < 0; y > 0", "x < 0; y < 0", "x > 0; y < 0"};
+if (flat == 1)
+{
+    System.Console.WriteLine($"Для указанной четверти возможны следующие координаты: {part[0]}");
+}
+else if (flat == 2)
+{
+    System.Console.WriteLine($"Для указанной четверти возможны следующие координаты: {part[1]}");
+}
+else if (flat == 3)
+{
+    System.Console.WriteLine($"Для указанной четверти возможны следующие координаты: {part[2]}");
+}
+else if (flat == 4)
+{
+    System.Console.WriteLine($"Для указанной четверти возможны следующие координаты: {part[3]}");
+
+}
+else
+{
+    System.Console.WriteLine("В двухмерном пространстве только 4 плоскости координат");
+}
