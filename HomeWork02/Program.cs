fb21c5f3316afc19f40f48a04536fa80b918829b
VladimirVysochanskiy <shunkar78@mail.ru>
2023-01-07 19:49:07 +0300

    Nullable fixed

--- HomeWork02/Program.cs
+++ HomeWork02/Program.cs
@@ -6,10 +6,10 @@ A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
 Console.Clear();
 System.Console.Write("Введите координаты точки А через пробел (x y z): ");
-string? txtA = Console.ReadLine();
+string txtA = Console.ReadLine();
 double[] a = txtA.Split().Select(double.Parse).ToArray();
 System.Console.Write("Введите координаты точки B через пробел (x y z): ");
-string? txtB = Console.ReadLine();
+string txtB = Console.ReadLine();
 double[] b = txtB.Split().Select(double.Parse).ToArray();
 
 System.Console.WriteLine($"Расстояние между точками: {Math.Sqrt(Math.Pow(b[0] - a[0], 2) + Math.Pow(b[1] - a[1], 2) + Math.Pow(b[2] - a[2], 2))}");
